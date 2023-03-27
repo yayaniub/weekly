2fde48a2f6410f9c8b18c307658eacfa1e374646
yayaniub <106913111+yayaniub@users.noreply.github.com>
2023-03-27 16:35:45 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,9 +14,9 @@ posts.splice(15);
 
 export const get = () =>
   rss({
-    title: '潮流周刊',
-    description: '记录 Tw93 潮流前端的日常生活',
-    site: 'https://weekly.tw93.fun/',
+    title: '瞎玩周刊',
+    description: '记录 PGY 潮流前端的日常生活',
+    site: 'https://weekly.pgyu.org/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
